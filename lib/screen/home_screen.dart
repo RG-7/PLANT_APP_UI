404fb4e018af460980408f7b6a4fde95310d8643
Ratn Govindam <rg.ratn@gmail.com>
2024-09-09 15:48:56 +0530

    update home screen 1

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,190 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app_ui/screen/product.dart';
+import 'package:plant_app_ui/utils/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Row(
+            children: [
+              sideBar(),
+              bodyParts(size),
+            ],
+          ),
+          const Positioned(
+            top: 135,
+            left: 29,
+            child: Text(
+              "Plants",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 35,
+              ),
+            ),
+          ),
+          const Positioned(
+            top: 45,
+            right: 8,
+            child: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 40,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container bodyParts(Size size) {
+    return Container(
+      width: size.width * 0.7,
+      color: Colors.white,
+      child: const SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Hero(
+              tag: 'Flower1',
+              child: Product(
+                name: "House Shape Close",
+                price: "45",
+                image: "images/flower1.png",
+              ),
+            ),
+            Hero(
+              tag: 'Flower2',
+              child: Product(
+                name: "Glass Water",
+                price: "60",
+                image: "images/flower2.png",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded sideBar() {
+    return Expanded(
+        child: Container(
+      color: secondaryColor,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8,
+              top: 38,
+            ),
+            child: Image.asset(
+              "images/more icon.png",
+              width: 60,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: Text(
+              "Green",
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          const SizedBox(
+            height: 210,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 14),
+            child: RotatedBox(
+              quarterTurns: 3,
+              child: RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                    text: ".",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Shape Class",
+                    style: TextStyle(
+                      color: primaryColor,
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+          ),
+          // sizedbox
+          const SizedBox(
+            height: 40,
+          ),
+
+          const Padding(
+            padding: EdgeInsets.only(left: 8),
+            child: RotatedBox(
+              quarterTurns: 3,
+              child: Text(
+                "Indore Plants",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+
+          // sizedbox
+          const SizedBox(
+            height: 30,
+          ),
+
+          const Padding(
+            padding: EdgeInsets.only(left: 8),
+            child: RotatedBox(
+              quarterTurns: 3,
+              child: Text(
+                "Green Plants",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 16, top: 110),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                color: primaryColor,
+              ),
+              width: 60,
+              height: 70,
+              child: const Icon(
+                Icons.home,
+                color: Colors.white,
+                size: 40,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
