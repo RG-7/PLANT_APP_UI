3ba7bb85a36e5912704b587e0e725af2e0b5cdf8
Ratn Govindam <rg.ratn@gmail.com>
2024-09-09 15:48:16 +0530

    add product

---
+++ lib/screen/product.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Product extends StatelessWidget {
+  final String name;
+  final String price;
+  final String image;
+  const Product(
+      {super.key,
+      required this.name,
+      required this.price,
+      required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(
+          image,
+          width: 300,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 55,
+          ),
+          child: Text(
+            name,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(
+            left: 55,
+            right: 20,
+          ),
+          child: Text(
+            "More details about the product, that customers wants to know",
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Padding(
+          padding:const EdgeInsets.only(
+            left: 54,
+            right: 17,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "\$$price",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Image.asset("images/basket icon.png",height: 60,width: 60,),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
